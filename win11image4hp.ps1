b336e35013216175ef705cfbb3bbcf152cbf49a8
amiseh <82850484+amiseh@users.noreply.github.com>
2025-08-19 22:07:49 +0200

    Update win11image4hp.ps1

--- win11image4hp.ps1
+++ win11image4hp.ps1
@@ -191,5 +191,11 @@ Remove-Item -Path C:\Drivers\ -Recurse -Force
 #### adding some OOBE configuration
 dism.exe /image:C: /Apply-Unattend:D:\unattended-basic-config.xml
 
+#xcopy D:\unattended-basic-config.xml c:\Windows\System32\Sysprep\Panther\Unattend\Unattend.xml /-I
+#xcopy D:\unattended-basic-config.xml c:\Windows\System32\Sysprep\Unattend.xml /-I
+#xcopy D:\unattended-basic-config.xml c:\Windows\System32\Sysprep\Panther\Unattend\Autounattend.xml /-I
+#xcopy D:\unattended-basic-config.xml c:\Windows\System32\Sysprep\Autounattend.xml /-I
+
 #Restart
 restart-computer
+
